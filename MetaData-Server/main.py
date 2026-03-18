a4a29b2495e33b32630792c6d3f08b032d56505c
JKeanu2503 <keanu.hock@gmail.com>
2026-03-18 20:49:01 +0100

    token generation

--- MetaData-Server/main.py
+++ MetaData-Server/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 import models, schemas, security
 from database import engine, get_db
+from fastapi.security import OAuth2PasswordRequestForm
 
 app = FastAPI(title="DIESE DATEI BEARBEITE ICH GERADE")
 
@@ -32,3 +33,17 @@ def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)  # Holt die neue ID aus der DB
 
     return new_user
+
+@app.post("/login")
+def login(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
+    # 1. User suchen
+    user = db.query(models.Account).filter(models.Account.username == form_data.username).first()
+
+    # 2. Existiert der User und stimmt das Passwort?
+    if not user or not security.pwd_context.verify(form_data.password, user.hashed_pw):
+        raise HTTPException(status_code=401, detail="Falscher Username oder Passwort")
+
+    # 3. Token erstellen
+    access_token = security.create_access_token(data={"sub": user.username, "id": user.account_id})
+
+    return {"access_token": access_token, "token_type": "bearer"}
--- MetaData-Server/security.py
+++ MetaData-Server/security.py
@@ -1,3 +1,5 @@
+import jwt
+from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
 
 # Wir sagen passlib, dass wir den "bcrypt"-Algorithmus nutzen wollen.
@@ -11,3 +13,15 @@ def get_password_hash(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Prüft beim Login, ob das getippte Passwort zum Salat in der DB passt."""
     return pwd_context.verify(plain_password, hashed_password)
+
+# Das ist dein geheimer Schlüssel – in echt sollte der in einer .env Datei stehen!
+SECRET_KEY = "DEIN_SUPER_GEHEIMER_SCHLÜSSEL_VON_AWAVE"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 # Token ist 24h gültig
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
