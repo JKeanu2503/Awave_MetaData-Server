89db1884ac832258675f0ffe8473cb65bb4b09dc
JKeanu2503 <keanu.hock@gmail.com>
2026-03-19 06:31:42 +0100

    prepare for update to SQLAlchemy 2.0 Code

--- MetaData-Server/database.py
+++ MetaData-Server/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 # Die URL kennst du schon - echo=True hilft dir beim Lernen (SQL-Log)
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Dasselt12@localhost:5432/postgres"
@@ -7,7 +7,8 @@ SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Dasselt12@localhost:5432/postgr
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 # Diese Funktion gibt uns eine saubere Verbindung für jeden Request
 def get_db():
